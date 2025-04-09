e47603b1dd35e0aed1d18be6f661a366ca8d3a68
Jason Ha <jha10@wgu.edu>
2025-04-09 15:28:19 -0500

    Added FastAPI routes and client requests for local testing

--- local_api.py
+++ local_api.py
@@ -1,17 +1,15 @@
 import json
-
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+# === GET Request ===
+# Send a GET using the URL http://127.0.0.1:8000
+r = requests.get("http://127.0.0.1:8000")
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+# Print the status code and welcome message
+print("GET Status Code:", r.status_code)
+print("GET Response:", r.json())
 
-
-
+# === POST Request ===
 data = {
     "age": 37,
     "workclass": "Private",
@@ -29,10 +27,9 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+# Send a POST using the data above
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+# Print the status code and result
+print("POST Status Code:", r.status_code)
+print("POST Response:", r.json())
